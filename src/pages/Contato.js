cd31198463b4e10caff413c67a77079f7ab9912d
wenned <wenned@gmail.com>
2023-01-25 21:35:13 -0300

    Atualizacao area Contato

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -1,14 +1,28 @@
 import style from '../styles/Contato.module.css'
-// import {FaLinkedin, FaGithub, FaTwitter} from 'react-icons/fa'
+import {FaLinkedin, FaGithub, FaTwitter} from 'react-icons/fa'
 
 
 function Contato(){
     return (
         <div className={`${style.conteiner}`} id="Contato">
-            <h1 className={style.item} id="Contat">Contato</h1>
-            {/* <span><FaLinkedin/></span>
-            <span><FaGithub/></span>
-            <span><FaTwitter/></span> */}
+            <div className={style.social} id='Contat'>
+                <span className={style.l}><FaLinkedin/></span>
+                <span className={style.l}><FaGithub/></span>
+                <span className={style.l}><FaTwitter/></span>
+            </div>
+            <div className={style.habil}>
+                <span className={style.titulos} >Programação</span>
+                <span>JavaScript</span>
+                <span>Java</span>
+                <span>Python</span>
+                <div className={style.linha}></div>
+                <span className={style.titulos} >Desenvolvimento</span>
+                <span>Node.JS</span>
+                <span>Next.JS</span>
+                <span>React</span>
+                <span>Html</span>
+                <span>Css</span>
+            </div>
         </div>
     )
 }
