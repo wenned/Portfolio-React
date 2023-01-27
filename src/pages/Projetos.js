256d80463307a67d3d466c6ab0d0a61ba4cb1c19
wenned <wenned@gmail.com>
2023-01-27 15:50:31 -0300

    criacao de animacao

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,10 +1,18 @@
 import style from '../styles/Projetos.module.css'
 
 function Projetos(){
+
+
     return (
     
         <div className={style.conteiner} id="Projetos">
-            <h1 className={style.item} id="Project">Projetos</h1>
+
+            <div id="Elemento1" className={style.l1}></div>
+            <div id="Elemento2" className={style.l2}></div>
+
+            <div id="Elemento3" className={style.l3}></div>
+            <div id="Elemento4" className={style.l4}></div>
+
         </div>
         )
 }
