d937fb5653b1d6a639788afd52672ec917245b80
wenned <wenned@gmail.com>
2023-02-01 11:28:40 -0300

    atualizacao exibicao projeto

--- src/components/layout_projeto/LayoutProjeto.js
+++ src/components/layout_projeto/LayoutProjeto.js
@@ -6,9 +6,9 @@ function LayoutProjeto({cost1, cost2, cost3, cost4}){
 
         <>
             <div id="Elemento1" className={`${style.l1} ${style[cost1]}`}></div>
-            <div id="Elemento2" className={`${style.l2} ${style[cost2]}`}><span>B R E V E</span></div>
-            <div id="Elemento3" className={`${style.l3} ${style[cost3]}`}><span>B R E V E</span></div>
-            <div id="Elemento4" className={`${style.l4} ${style[cost4]}`}><span>B R E V E</span></div>
+            <div className={`${style.l2} ${style[cost2]}`}><div id="Elemento2" ></div><span>B R E V E</span></div>
+            <div className={`${style.l3} ${style[cost3]}`}><div id="Elemento3" ></div><span>B R E V E</span></div>
+            <div className={`${style.l4} ${style[cost4]}`}><div id="Elemento4" ></div><span>B R E V E</span></div>
         </>
     )
 };
