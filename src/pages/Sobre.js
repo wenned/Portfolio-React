c3fcb7f2e75e3d10da9f6a5a66349b7e2b380c9b
wenned <wenned@gmail.com>
2023-05-17 19:34:36 -0300

    atualizacao medias querys PROJETOS

--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -13,7 +13,7 @@ function Sobre(){
                         <br/>
                         <p>Sou uma pessoa dinâmica, aberto a novas experiências, sempre tentando ser pontual e cumprir compromissos, nascido em, <strong>Cândido Mendes, MA</strong>, mudei-me para <strong>Imperatriz, MA</strong> aos 16 anos, sempre com objetivo de me especializar na area da tecnologia, mesmo naquela época não sabendo para qual área realmente focar, sempre me adaptei facilmente em varias áreas da programação tanto, <strong>Programação Back-end</strong>, onde foi meu primeiro contato com programação, e agora nessa nova fase com <strong>Programação Fronte-end</strong></p>
                         <br/>
-                        <p><strong>Hobbies:</strong> Estudar programação, <strong>"JavaScript", "Python", "JAVA", "POSTGRESQL"</strong>. Desemvolvimento Web: <strong>"NODE.JS", "REACT", "NEXT.JS", "HTML", "CSS"</strong>.</p>
+                        <p><strong>Hobbies:</strong> Estudar programação, <strong>"JavaScript", "Python", "JAVA", "POSTGRESQL", "MongoDB"</strong>. Desemvolvimento Web: <strong>"NODE.JS", "REACT", "NEXT.JS", "HTML", "CSS"</strong>.</p>
                 </div>
 
                 <div >
@@ -29,11 +29,6 @@ function Sobre(){
                 <div>
                     <p>"Tenho trabalhado em projetos pessoais para desenvolver minhas habilidades em programação. Entre elas <strong>React, JavaScript, Python e Postgresql.</strong> Durante o processo, estou aprendendo a lidar com autenticação de usuários e armazenamento de dados. Além disso, fui capaz de solucionar problemas técnicos por conta própria, destacando minha capacidade de pensar de forma crítica e encontrar soluções. Estou animado para continuar aprendendo e aplicando meus conhecimentos em futuros projetos."</p>
                 </div>
-
-
-
-            {/* <div className={style.l2}></div>
-            <div className={style.v2}></div> */}
 
         </section>
         )
