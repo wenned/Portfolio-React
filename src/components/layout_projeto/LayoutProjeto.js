b10fa60ee6661b3d0e9c915d500a93ac8c8ce68d
wenned <wenned@gmail.com>
2023-07-14 19:17:27 -0300

    atualizacao novo padrao area projetos

--- src/components/layout_projeto/LayoutProjeto.js
+++ src/components/layout_projeto/LayoutProjeto.js
@@ -1,32 +1,86 @@
+import { useState } from 'react';
 import InfoProjeto from '../infoprojetos/InfoProjeto';
 import style from '../layout_projeto/LayoutProjeto.module.css'
 
-function LayoutProjeto({cost1, cost2, cost3, cost4 }){
 
+function LayoutProjeto(){
+
+
+    const [layout1, setLayout1] = useState('')
+    const [layout2, setLayout2] = useState('')
+    const [layout3, setLayout3] = useState('')
+    const [layout4, setLayout4] = useState('')
+
+  document.addEventListener('click', (e)=>{
+
+    switch (e.target.id){
+
+        case 'l1':
+            
+            if(layout1 === ''){
+                setLayout1('active');
+            }else(setLayout1(''))
+            
+            break
+
+
+        case 'Elemento2':
+
+            if(layout2 === ''){
+                setLayout2('active')
+            }else(setLayout2(''))
+            
+            break
+            
+        case 'Elemento3':
+
+            if(layout3 === ''){
+                setLayout3('active')
+            }else(setLayout3(''))
+            
+            break
+            
+        case 'Elemento4':
+
+            if(layout4 === ''){
+                setLayout4('active')
+            }else(setLayout4(''))
+            
+            break
+
+        default:
+            //no default
+    }
+
+})
     return(
  
-        <>
-            <div className={`${style.l1} ${style[cost1]}`}>
-                <div id="Elemento1" className={style.DivStyle} ></div>
-                <div ><InfoProjeto id='hy'/></div>
-                <span></span>
+        <div className={style.conteiner}>
+
+            <div className={`${style.l1} ${style[layout1]}`}>
+                <div className={`${style.nomeProjeto}`} id='l1'>PORTIFOLIO</div>
+                                
+                <span className={style.imagemProjeto}></span>
+
+                <div className={style.proJeto}>
+                    <InfoProjeto id='hy'/>
+                </div>
             </div>
 
-            <div className={`${style.l2} ${style[cost2]}`}>
-                <div id="Elemento2" className={style.DivStyle} ></div>
+            <div className={`${style.l2} ${style[layout2]}`}>
                 <span>B R E V E</span>
             </div>
 
-            <div className={`${style.l3} ${style[cost3]}`}>
+            <div className={`${style.l3} ${style[layout3]}`}>
                 <div id="Elemento3" className={style.DivStyle} ></div>
                 <span>B R E V E</span>
             </div>
             
-            <div className={`${style.l4} ${style[cost4]}`}>
+            <div className={`${style.l4} ${style[layout4]}`}>
                 <div id="Elemento4" className={style.DivStyle} ></div>
                 <span>B R E V E</span>
             </div>
-        </>
+        </div>
     )
 };
 
--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,63 +1,14 @@
 import style from '../styles/Projetos.module.css'
-import { useState } from "react";
 import LayoutProjeto from "../components/layout_projeto/LayoutProjeto";
 
 
 
 function Projetos(){
 
-    const [layout1, setLayout1] = useState('')
-    const [layout2, setLayout2] = useState('')
-    const [layout3, setLayout3] = useState('')
-    const [layout4, setLayout4] = useState('')
-
-  document.addEventListener('click', (e)=>{
-
-    switch (e.target.id){
-
-        case 'Elemento1':
-            
-            if(layout1 === ''){
-                setLayout1('active');
-            }else(setLayout1(''))
-            
-            break
-
-
-        case 'Elemento2':
-
-            if(layout2 === ''){
-                setLayout2('active')
-            }else(setLayout2(''))
-            
-            break
-            
-        case 'Elemento3':
-
-            if(layout3 === ''){
-                setLayout3('active')
-            }else(setLayout3(''))
-            
-            break
-            
-        case 'Elemento4':
-
-            if(layout4 === ''){
-                setLayout4('active')
-            }else(setLayout4(''))
-            
-            break
-
-        default:
-            //no default
-    }
-
-})
-
     return (
         <>
             <div className={style.conteiner} id="Projetos">
-                <LayoutProjeto cost1={layout1} cost2={layout2} cost3={layout3} cost4={layout4}/>
+                <LayoutProjeto/>
             </div>
         </>
         )
