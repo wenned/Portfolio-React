7e390aab76fbfff59da865304cf178ae19ddc4a5
wenned <wenned@gmail.com>
2023-09-24 16:15:18 -0300

    correcao de css e midasquerys Portifolio

--- src/components/infoprojetos/InfoProjeto.js
+++ src/components/infoprojetos/InfoProjeto.js
@@ -21,13 +21,25 @@ function InfoProjeto(){
                 <h1>Tecnologias Usadas:</h1>
 
                 <div className={style.skils}>
-                    <div><FaJs/>: JavaScript</div>
-                    <div><FaHtml5/>: Hypertext Markup Language</div>
-                    <div><FaCss3/>: Cascading Style Sheets</div>
+                    <div><FaJs/> : JavaScript</div>
+                    <div><FaHtml5/> : Hypertext Markup Language</div>
+                    <div><FaCss3/> : Cascading Style Sheets</div>
                 </div>
 
                 <div className={style.texto}>
-                    <p>"Primeiro projeto web desenvolvido com JavaScript, HTML e CSS puro, desenvolvido como trabalho para a faculdade, em aperfeiçoamento das habilidades, migrei o mesmo para React."</p>
+                    <p>
+                        "Comecei meu aprendizado em desenvolvimento web com um projeto que utilizou <strong>JavaScript</strong>, <strong>HTML</strong> e <strong>CSS</strong> puros como parte de um trabalho acadêmico. Esse projeto inicial me ajudou a compreender os fundamentos do desenvolvimento web e a aprimorar minhas habilidades.
+                    </p>
+
+                    <p>
+                        À medida que ganhei confiança e experiência, decidi migrar esse projeto para o framework React. Essa mudança me permitiu explorar uma abordagem mais avançada para criar interfaces de usuário interativas.
+                    </p>
+
+                    <p>
+                        Com o React, pude dividir a interface em componentes reutilizáveis, facilitando a manutenção e expansão do projeto.
+                        Uma das maiores vantagens do React é seu ecossistema robusto, que oferece várias bibliotecas e ferramentas para melhorar a produtividade do desenvolvedor e adicionar funcionalidades avançadas."
+                    </p>
+                    
                 </div>
 
                 <div className={style.link}><FaLink onClick={Falin}/> <p onClick={Falin}>: https://portfoliowenned.netlify.app/</p></div>
